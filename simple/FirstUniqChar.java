387368ed0eb4dcfc1e3325ff127b3877759abdd1
imyttizy <1664619620@qq.com>
2023-07-20 14:02:59 +0800

    feat: first unique char done

---
+++ simple/FirstUniqChar.java
@@ -0,0 +1,19 @@
+package simple;
+
+import java.util.HashMap;
+
+public class FirstUniqChar {
+
+
+    public char firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        for (int i = 0; i <s.length() ; i++) {
+            if (map.get(s.charAt(i))==1) return s.charAt(i);
+        }
+        return ' ';
+    }
+
+}
