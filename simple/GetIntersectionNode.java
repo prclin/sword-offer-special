6bcd73c14bfda9cda95ebe62c55204f47a6269d9
imyttizy <1664619620@qq.com>
2023-07-20 14:39:08 +0800

    feat: get intersection node done

---
+++ simple/GetIntersectionNode.java
@@ -0,0 +1,24 @@
+package simple;
+
+public class GetIntersectionNode {
+
+    ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA==null||headA==null) return null;
+        ListNode p1=headA,p2=headB;
+        while (p1!=p2){
+            p1=p1==null?headB:p1.next;
+            p2=p2==null?headA:p2.next;
+        }
+        return p1;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
