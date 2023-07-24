e6b95fbedd6e44178f82911080b767c8bc4c3ee3
imyttizy <1664619620@qq.com>
2023-07-24 10:26:36 +0800

    fix: fix last remaining done

--- simple/LastRemaining.java
+++ simple/LastRemaining.java
@@ -7,17 +7,11 @@ import java.util.concurrent.ArrayBlockingQueue;
 public class LastRemaining {
 
     public int lastRemaining(int n, int m) {
-        Queue<Integer> queue=new ArrayBlockingQueue(n);
-        for (int i = 0; i < n; i++) {
-            queue.add(i);
+        int f = 0;
+        for (int i = 2; i != n + 1; ++i) {
+            f = (m + f) % i;
         }
-        while (queue.size()!=1){
-            for (int i = 0; i < m; i++) {
-                if (i==m-1) queue.poll();
-                else queue.add(queue.poll());
-            }
-        }
-        return queue.poll();
+        return f;
     }
 
 }
